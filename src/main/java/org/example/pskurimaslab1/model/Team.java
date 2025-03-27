ad284bc81c04c646ddc8cbc2cc5002a734f7298c
Buztas <justas.buzys@gmail.com>
2025-03-27 19:54:55 +0200

    feat: jsf proper integration start

--- src/main/java/org/example/pskurimaslab1/model/Team.java
+++ src/main/java/org/example/pskurimaslab1/model/Team.java
@@ -1,18 +1,19 @@
 package org.example.pskurimaslab1.model;
 
 import jakarta.persistence.*;
-
+import java.io.Serializable;
 import java.util.List;
 
 @Entity
-public class Team {
+public class Team implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String sport;
 
-    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Player> players;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
@@ -23,43 +24,31 @@ public class Team {
     )
     private List<Tournament> tournaments;
 
-    public Long getId() {
-        return id;
-    }
+    public Team() {}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getSport() { return sport; }
+    public void setSport(String sport) { this.sport = sport; }
 
-    public String getSport() {
-        return sport;
-    }
+    public List<Player> getPlayers() { return players; }
+    public void setPlayers(List<Player> players) { this.players = players; }
 
-    public void setSport(String sport) {
-        this.sport = sport;
-    }
+    public List<Tournament> getTournaments() { return tournaments; }
+    public void setTournaments(List<Tournament> tournaments) { this.tournaments = tournaments; }
 
-    public List<Player> getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(List<Player> players) {
-        this.players = players;
-    }
-
-    public List<Tournament> getTournaments() {
-        return tournaments;
-    }
-
-    public void setTournaments(List<Tournament> tournaments) {
-        this.tournaments = tournaments;
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sport='" + sport + '\'' +
+                ", players=" + (players != null ? players.size() : "null") +
+                ", tournaments=" + (tournaments != null ? tournaments.size() : "null") +
+                '}';
     }
 }
